47ba416b0e7a0f056b96ced77a3ac1ddcc094a4e
kanuri-suresh <123804358+kanuri-suresh@users.noreply.github.com>
2025-02-08 13:08:01 +0530

    src, mvn folders added

---
+++ src/main/java/com/suresh_training/suresh_training/SureshTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.suresh_training.suresh_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SureshTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SureshTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suresh_training/suresh_training/SureshTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh_training.suresh_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SureshTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
